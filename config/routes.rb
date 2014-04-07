878b5f93a042984df2d54e18f418ee32d9d42021
Matt Lenz <matt@mattlenz.com>
2014-04-07 11:24:29 +1000

    Section as sub-resource of BrandGuide

--- config/routes.rb
+++ config/routes.rb
@@ -6,6 +6,8 @@ BMS::Application.routes.draw do
   namespace :admin do
     root to: 'brand_guides#index'
 
-    resources :brand_guides
+    resources :brand_guides do
+      resources :sections
+    end
   end
 end
