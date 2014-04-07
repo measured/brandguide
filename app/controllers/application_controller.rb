2a6618dc61cfb64376dfa7bd2546be75aa3a1e77
Matt Lenz <matt@mattlenz.com>
2014-04-07 10:55:15 +1000

    Show admin index after sign-in

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,4 +4,8 @@ class ApplicationController < ActionController::Base
   before_action :authenticate_user!
 
   def index; end
+
+  def after_sign_in_path_for(resource)
+    admin_root_path
+  end
 end
