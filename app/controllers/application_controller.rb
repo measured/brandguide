ba6dc4bdfb0bdb2c76453eeb36d0e171d273af78
Matt Lenz <matt@mattlenz.com>
2014-04-07 12:02:11 +1000

    Requrie sign-in on /admin

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,8 +1,6 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
 
-  before_action :authenticate_user!
-
   def index; end
 
   def after_sign_in_path_for(resource)
