2d23ffa540beee78ae156052670f58d5f6b59f06
Matt Lenz <matt@mattlenz.com>
2014-04-10 14:26:07 +1000

    Do this over GET instead of a form POST

--- app/controllers/asset_groups_controller.rb
+++ app/controllers/asset_groups_controller.rb
@@ -4,11 +4,12 @@ class AssetGroupsController < ApplicationController
     # also, that the assets truly belong to that BrandGuide
     @asset_group = AssetGroup.find(params[:id])
 
-    assets = asset_group_params[:assets_attributes].map do |n, param|
-      @asset_group.assets.find(param[:id]) if param[:download] == '1'
+    if params[:asset_ids]
+      asset_ids = params[:asset_ids].split(',').map(&:to_i)
+      assets = @asset_group.assets.find(asset_ids)
+    else
+      assets = @asset_group.assets
     end
-
-    assets.compact!
 
     zip_name = [
       @asset_group.brand_guide.title,
@@ -26,37 +27,4 @@ class AssetGroupsController < ApplicationController
 
     send_file tempfile.path, type: 'application/zip', disposition: :attachment, filename: zip_name
   end
-
-  private
-
-  def asset_group_params
-    params.require(:asset_group).permit(:id, assets_attributes: [:download, :id])
-  end
 end
-
-# def download_all
-# attachments = Upload.find(:all, :conditions => ["source_id = ?", params[:id]]) 
-
-# zip_file_path = "#{RAILS_ROOT}/uploads/download_all.zip"
-
-
-# # see if the file exists already, and if it does, delete it.
-# if File.file?(zip_file_path)
-# File.delete(zip_file_path)
-# end
-
-
-# # open or create the zip file
-# Zip::ZipFile.open(zip_file_path, Zip::ZipFile::CREATE) { |zipfile|
-
-# attachments.each do |attachment|
-# #document_file_name shd contain filename with extension(.jpg, .csv etc) and url is the path of the document.
-# zipfile.add( attachment.document_file_name, attachment.document.url) 
-
-# end
-
-# } 
-# #send the file as an attachment to the user.
-# send_file zip_file_path, :type => 'application/zip', :disposition => 'attachment', :filename => "download_all.zip"
-
-# end
