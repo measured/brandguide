62c5acca3fa9424a301030ee9a05e51f308c9cbc
Matt Lenz <matt@mattlenz.com>
2014-04-15 14:47:58 +1000

    Bundle jquery-ui

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -2,9 +2,10 @@
 //= require 02-jquery
 //= require 03-transit
 
+//= require jquery_ujs
+//= require jquery.ui.all
+
 //= require scripts/00-vars
 //= require scripts/01-events
 //= require scripts/02-utilities
 //= require scripts/03-breakpoints
-
-//= require jquery_ujs
