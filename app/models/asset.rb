3805f2543d333770be69cae0e2107c4981598944
Matt Lenz <matt@mattlenz.com>
2014-04-22 18:07:26 +1000

    Fix image asset detection

--- app/models/asset.rb
+++ app/models/asset.rb
@@ -2,7 +2,8 @@ class Asset < ActiveRecord::Base
   dragonfly_accessor :file
   belongs_to :asset_group
 
-  scope :images, -> { where(file_image: true) }
+  # scope :images, -> { where(file_image: true) }
+  scope :images, -> { where(file_format: [:jpeg, :jpg, :gif, :png]) }
 
   attr_accessor :download
 end
