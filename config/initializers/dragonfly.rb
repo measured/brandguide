1c45fe7d8d38bb2cd93a3c2bee7c7f627a0e4bcc
Matt Lenz <matt@mattlenz.com>
2014-04-24 15:06:59 +1000

    Store attachments on s3

--- config/initializers/dragonfly.rb
+++ config/initializers/dragonfly.rb
@@ -1,17 +1,25 @@
 require 'dragonfly'
+require 'dragonfly/s3_data_store'
 
 # Configure
 Dragonfly.app.configure do
   plugin :imagemagick
 
   protect_from_dos_attacks true
-  secret "7b922bcd75f757e34d63d17514f521740381e408bb655e48792ef041af9b33a4"
+  secret '7b922bcd75f757e34d63d17514f521740381e408bb655e48792ef041af9b33a4'
 
-  url_format "/media/:job/:name"
+  url_format '/media/:job/:name'
 
-  datastore :file,
-    root_path: Rails.root.join('public/system/dragonfly', Rails.env),
-    server_root: Rails.root.join('public')
+  if Rails.env.production?
+    datastore :s3,
+      bucket_name: ENV['AWS_S3_BUCKET'],
+      access_key_id: ENV['AWS_ACCESS_KEY_ID'],
+      secret_access_key: ENV['AWS_SECRET_ACCESS_KEY']
+  else
+    datastore :file,
+      root_path: Rails.root.join('public/system/dragonfly', Rails.env),
+      server_root: Rails.root.join('public')
+  end
 end
 
 # Logger
