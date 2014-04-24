a919b3b9b7ff186395b932621614d1cb1e9ea005
Matt Lenz <matt@mattlenz.com>
2014-04-24 15:29:35 +1000

    Section ancestry selection

--- app/controllers/admin/sections_controller.rb
+++ app/controllers/admin/sections_controller.rb
@@ -1,9 +1,23 @@
 class Admin::SectionsController < Admin::AdminController
+  before_action :find_brand_guide, only: [:new, :edit, :create, :update]
   before_action :find_section, only: [:edit, :update]
 
-  def new; end
+  def new
+    @section = Section.new
+  end
 
   def edit; end
+
+  def create
+    @section = Section.new(section_params)
+    @section.brand_guide = @brand_guide
+
+    if @section.save
+      redirect_to edit_admin_brand_guide_path(@brand_guide)
+    else
+      render :new
+    end
+  end
 
   def update
     if files = section_params[:files]
@@ -22,7 +36,7 @@ class Admin::SectionsController < Admin::AdminController
     end
 
     if @section.update(section_params)
-      redirect_to [:edit, :admin, @brand_guide, @section]
+      redirect_to edit_admin_brand_guide_section_path(@brand_guide, @section)
     else
       render :edit
     end
@@ -30,12 +44,15 @@ class Admin::SectionsController < Admin::AdminController
 
   private
 
+  def find_brand_guide
+    @brand_guide = BrandGuide.friendly.find(params[:brand_guide_id])
+  end
+
   def find_section
-    @brand_guide = BrandGuide.find(params[:brand_guide_id])
     @section = @brand_guide.sections.find(params[:id])
   end
 
   def section_params
-    params.require(:section).permit(:title, :content, asset_groups_attributes: [:id, :title, :_destroy], files: [])
+    params.require(:section).permit(:title, :parent_id, :content, asset_groups_attributes: [:id, :title, :_destroy], files: [])
   end
 end
--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,12 @@
 module ApplicationHelper
+  def ancestry_collection(items)
+    result = []
+    
+    items.map do |item, sub_items|
+        result << [('- ' * item.depth) + item.title, item.id]
+        result += ancestry_collection(sub_items) unless sub_items.blank?
+    end
+
+    result
+  end
 end
