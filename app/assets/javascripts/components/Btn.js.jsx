522ca8452afec40d4f63c9cf0c08902edeb56b24
Matt Lenz <matt@mattlenz.com>
2014-04-25 12:49:21 +1000

    "disable" functionality for Btn

--- app/assets/javascripts/components/Btn.js.jsx
+++ app/assets/javascripts/components/Btn.js.jsx
@@ -3,13 +3,24 @@
 // I wanted to call this "<Button/>", but the name clashes with an existing HTML tag
 
 var Btn = React.createClass({
+  getDefaultProps: function() {
+    return {
+      onClick: function() {},
+      disabled: false
+    }
+  },
   onClick: function(event) {
     event.preventDefault();
     this.props.onClick();
   },
   render: function() {
+    var btnClass = React.addons.classSet({
+      Btn: true,
+      disabled: this.props.disabled
+    });
+
     return this.transferPropsTo(
-      <a className="Btn" href="#" onClick={this.onClick}>
+      <a className={btnClass} href="#" onClick={this.onClick}>
         <i className="ss-icon">{this.props.icon}</i><span>{this.props.children}</span>
       </a>
     );
