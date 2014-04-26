05ec46f5e04b159433297b690710cc1540c41511
Matt Lenz <matt@mattlenz.com>
2014-04-26 16:34:49 +1000

    Inherit find_brand_guide

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,5 +1,4 @@
 class PagesController < BrandGuidesController
-  before_action :find_brand_guide
   before_action :find_page, only: [:show]
 
   layout 'guide'
@@ -7,10 +6,6 @@ class PagesController < BrandGuidesController
   def show; end
 
   private
-
-  def find_brand_guide
-    @brand_guide = BrandGuide.friendly.find(request.subdomain)
-  end
 
   def find_page
     @page = @brand_guide.pages.friendly.find(params[:id])
