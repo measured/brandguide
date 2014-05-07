0193b13834730f46de6a22bec318c1e6386bd851
Matt Lenz <matt@mattlenz.com>
2014-05-07 12:38:40 +1000

    Commit before branch

--- app/assets/javascripts/components/BrandGuideEditor.js.jsx
+++ app/assets/javascripts/components/BrandGuideEditor.js.jsx
@@ -2,6 +2,64 @@
 
 //= require ./Modal
 
+var SectionEditor = React.createClass({
+  handleTitleChange: function(event) {
+    this.props.section.set('title', event.target.value);
+    this.props.guide.trigger('change');
+  },
+  handleContentChange: function(event) {
+    this.props.section.set('content', event.target.value);
+    this.props.guide.trigger('change');
+  },
+  handleSubmit: function(event) {
+    event.preventDefault();
+    this.props.guide.save();
+  },
+  render: function() {
+    return (
+      <div key={this.props.section.get('id')}>
+        <form onSubmit={this.handleSubmit}>
+          <input type="text" value={this.props.section.get('title')} onChange={this.handleTitleChange} />
+          <textarea onChange={this.handleContentChange}>{this.props.section.get('content')}</textarea>
+          {this.props.section.get('assetGroups')}
+        </form>
+      </div>
+    );
+  }
+});
+
+var EditorContent = React.createClass({
+  render: function() {
+    var self = this;
+
+    var editorPages = this.props.guide.get('pages').map(function(page) {
+      var pageSections = page.get('sections').map(function(section) {
+        return (
+          <SectionEditor
+            key={section.get('id')}
+            guide={self.props.guide}
+            page={page}
+            section={section} />
+        );
+      });
+
+      return (
+        <div key={page.id}>
+          <h1>{page.get('title')}</h1>
+          {page.get('content')}
+          {pageSections}
+        </div>
+      );
+    });
+
+    return (
+      <div className="EditorContent editor__content">
+        {editorPages}
+      </div>
+    )
+  }
+});
+
 var EditorTree = React.createClass({
   getInitialState: function() {
     return {
@@ -18,7 +76,14 @@ var EditorTree = React.createClass({
   },
   handleSettingsSubmit: function(event) {
     event.preventDefault();
-    this.props.guide.save();
+
+    var self = this;
+
+    this.props.guide.save({}, {
+      success: function() {
+        self.setState({ settingsOpen: false });
+      }
+    });
   },
   toggleTree: function(event) {
     event.preventDefault();
@@ -57,10 +122,10 @@ var EditorTree = React.createClass({
 
     var pageList = this.props.guide.get('pages').map(function(page) {
       var sections = page.get('sections').map(function(section) {
-        var editSectionUrl = '/'+self.props.guide.get('slug')+'/pages/'+page.get('slug')+'/'+section.id+'/edit';
+        var editSectionUrl = '/'+self.props.guide.get('slug')+'/pages/'+page.get('slug')+'/'+section.get('id')+'/edit';
 
-        return (<li key={section.id} className="tree__section">
-          <a href={editSectionUrl}>{section.title}</a>
+        return (<li key={section.get('id')} className="tree__section">
+          <a href={editSectionUrl}>{section.get('title')}</a>
         </li>);
       });
 
@@ -98,6 +163,7 @@ var EditorTree = React.createClass({
       <div className={settingsClassSet}>
         <form onSubmit={this.handleSettingsSubmit}>
           <input type="text" value={this.props.guide.get('title')} onChange={this.handleTitleChange} />
+          <input type="submit" />
         </form>
       </div>
 
@@ -113,10 +179,38 @@ var EditorTree = React.createClass({
   }
 });
 
+var BrandGuideSection = Backbone.Model.extend({
+  defaults: {
+    title: 'Untitled',
+    assetGroups: []
+  }
+});
+
+var BrandGuideSectionsCollection = Backbone.Collection.extend({
+  model: BrandGuideSection
+});
+
 var BrandGuidePage = Backbone.Model.extend({
   defaults: {
     title: 'Untitled',
     sections: []
+  },
+  parse: function(response) {
+    response.sections = new BrandGuideSectionsCollection(response.sections);
+  },
+  set: function(attributes, options) {
+    if(attributes.sections !== undefined && !(attributes.sections instanceof BrandGuideSectionsCollection)) {
+      attributes.sections = new BrandGuideSectionsCollection(attributes.sections);
+    }
+
+    return Backbone.Model.prototype.set.call(this, attributes, options);
+  },
+  toJSON: function() {
+    var json = this.attributes;
+
+    json.sections_attributes = _.clone(json.sections.toJSON());
+
+    return json;
   }
 });
 
@@ -125,6 +219,10 @@ var BrandGuidePagesCollection = Backbone.Collection.extend({
 });
 
 var BrandGuideModel = Backbone.Model.extend({
+  defaults: {
+    title: 'Untitled',
+    pages: []
+  },
   url: function() {
     return '/'+this.get('slug')+'.json'
   },
@@ -139,9 +237,29 @@ var BrandGuideModel = Backbone.Model.extend({
 
     return Backbone.Model.prototype.set.call(this, attributes, options);
   },
-  defaults: {
-    title: 'Untitled',
-    pages: []
+  toJSON: function() {
+    var json = this.attributes;
+    var pages = _.clone(json.pages.toJSON());
+
+    json.sections_attributes = [];
+
+    pages.forEach(function(page) {
+      var sections = _.clone(page.sections_attributes);
+
+      json.sections_attributes.push(page);
+
+      sections.forEach(function(section) {
+        json.sections_attributes.push(section);
+      });
+    });
+
+    console.log({
+      brand_guide: json
+    });
+
+    return {
+      brand_guide: json
+    }
   }
 });
 
@@ -190,9 +308,7 @@ var BrandGuideEditor = React.createClass({
   render: function() {
     return (<div className="BrandGuideEditor editor">
       <EditorTree modal={this.modal} guide={this.state.model} />
-      <div className="editor__content">
-        Editor
-      </div>
+      <EditorContent guide={this.state.model} />
       <Modal onClose={this.modal.close} data={this.state.modal} />
     </div>);
   }
