ac67e77317835b20993e3904713333e4b6281b8b
Matt Lenz <matt@mattlenz.com>
2014-05-09 12:22:21 +1000

    Use bower for vendor javascripts

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -1,8 +1,9 @@
-//= require jquery
-//= require underscore
-//= require react
-//= require react_ujs
-//= require showdown
+//= require bower_components/jquery/dist/jquery
+//= require bower_components/underscore/underscore
+//= require bower_components/showdown/compressed/showdown
+//= require bower_components/react/react-with-addons
+//= require bower_components/react-async/react-async
+//= require bower_components/react-router-component/react-router-component
 
 //= require_tree ./components
 
