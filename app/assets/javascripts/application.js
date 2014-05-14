ea84c1cb4064c4d0121114ee8f823ca5883996ed
Matt Lenz <matt@mattlenz.com>
2014-05-14 16:20:05 +1000

    Add dependencies

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -1,5 +1,8 @@
+//= require bower_components/eventEmitter/EventEmitter.js
+//= require bower_components/moment/moment
 //= require bower_components/jquery/dist/jquery
 //= require bower_components/underscore/underscore
+//= require bower_components/backbone/backbone
 //= require bower_components/showdown/compressed/showdown
 //= require bower_components/react/react-with-addons
 //= require bower_components/react-async/react-async
