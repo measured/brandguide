d8e41580149f9a87d0f5bb733ff4aef4f5b09b4c
Matt Lenz <matt@mattlenz.com>
2014-05-14 16:39:00 +1000

    Token authentication

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,7 +4,36 @@ class User < ActiveRecord::Base
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
 
-  def gravatar_url
-    "//gravatar.com/avatar/#{Digest::MD5.hexdigest(email)}"
+  before_save :ensure_authentication_token
+
+  has_many :guides
+
+  def ensure_authentication_token
+    if authentication_token.blank?
+      self.authentication_token = generate_authentication_token
+    end
+  end
+
+  def gravatar
+    "http://gravatar.com/avatar/#{Digest::MD5.hexdigest(email)}"
+  end
+
+  def api_attributes
+    {
+      email: email,
+      gravatar: gravatar,
+      token: authentication_token,
+      ctime: created_at.to_i,
+      mtime: updated_at.to_i
+    }
+  end
+
+  private
+
+  def generate_authentication_token
+    loop do
+      token = Devise.friendly_token
+      break token unless User.where(authentication_token: token).present?
+    end
   end
 end
