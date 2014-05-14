df336c157d2eaa8a92d7de3979898653030a2880
Matt Lenz <matt@mattlenz.com>
2014-05-14 16:40:08 +1000

    Simulate slow network on development

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,9 +1,12 @@
 class ApplicationController < ActionController::Base
   # protect_from_forgery with: :exception
+  before_action :slow
 
   def index; end
 
-  def after_sign_in_path_for(resource)
-    admin_root_url(subdomain: 'admin')
+  private
+
+  def slow
+    # sleep(rand(0.0..1.0)) if Rails.env.development?
   end
 end
