ede47330e75f74212b0d0f38a5c86734390c89f6
Matt Lenz <matt@mattlenz.com>
2014-05-28 14:46:36 +1000

    Configure for assets

--- config/application.rb
+++ config/application.rb
@@ -25,7 +25,5 @@ module BMS
     config.action_mailer.default_url_options = { host: 'brandguide.io' }
 
     config.browserify_rails.commandline_options = ['-t reactify']
-
-    config.assets.precompile += ['guide.js', 'guide.css']
   end
 end
--- config/environments/production.rb
+++ config/environments/production.rb
@@ -59,7 +59,8 @@ BMS::Application.configure do
 
   # Precompile additional assets.
   # application.js, application.css, and all non-JS/CSS in app/assets folder are already added.
-  config.assets.precompile += %w( guide.js guide.css )
+  config.assets.precompile += %w(guide.js guide.css)
+  config.assets.initialize_on_precompile = false
 
   # Ignore bad email addresses and do not raise email delivery errors.
   # Set this to true and configure the email server for immediate delivery to raise delivery errors.
