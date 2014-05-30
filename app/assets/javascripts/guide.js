224c1ea90f3c5a75922f1097afe2de8887496f34
Matt Lenz <matt@mattlenz.com>
2014-05-30 17:46:03 +1000

    Attempt to fix js bug

--- app/assets/javascripts/guide.js
+++ app/assets/javascripts/guide.js
@@ -8,21 +8,25 @@ var React = require('react');
 var ColoursList = require('./components/ColoursList');
 var AssetGroupsList = require('./components/AssetGroupsList');
 
-$('nav.sections').fixedsticky().on('click', 'a', function(event) {
+$(document).ready(function() {
+  $('nav.sections').fixedsticky();
+
+  $('nav.sections a').on('click', function(event) {
   event.preventDefault();
 
   $anchor = $(event.target.hash);
 
   $('html, body').animate({ scrollTop: $anchor.offset().top }, 500, $.bez([0.190, 1.000, 0.220, 1.000]));
-});
+  });
 
-var tinycolor = require('tinycolor2');
+  var tinycolor = require('tinycolor2');
 
-var primary = tinycolor(window.guide.primary);
+  var primary = tinycolor(window.guide.primary);
 
-$('header#banner').css({
-  backgroundColor: '#'+primary.toHex(),
-  color: tinycolor.mostReadable(primary, ['white', 'black'])
+  $('header#banner').css({
+    backgroundColor: '#'+primary.toHex(),
+    color: tinycolor.mostReadable(primary, ['white', 'black'])
+  });
 });
 
 // Make browserify and react-rails play nicely
